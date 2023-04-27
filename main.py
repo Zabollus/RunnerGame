b4a7fc2a40213be475cbdcb4589a903dc178131d
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2023-04-27 19:25:15 +0200

    collisions

--- main.py
+++ main.py
@@ -22,15 +22,22 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEMOTION:
+            if player_rect.collidepoint(event.pos) :
+                print('collision')
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+
     snail_rect.left -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
     screen.blit(snail_surf, snail_rect)
     screen.blit(player_surf, player_rect)
+
+    if player_rect.colliderect(snail_rect):
+        print('collision')
 
     pygame.display.update()
     clock.tick(60)
